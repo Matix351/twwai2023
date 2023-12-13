9d388ff934e65ff2a4b4852ce88a210dd771e665
Matix351 <130908742+Matix351@users.noreply.github.com>
2023-12-13 12:17:38 +0100

    dziala

--- index.js
+++ index.js
@@ -12,12 +12,22 @@
 
 const express = require('express');
 const questions = require('./questions').questions;
+const servPort = require('./config');
 
 const app = express();
 app.get('/api/questions', (request, response) => {
     response.send(questions);
  });
- 
+
+ app.get('/api/questions/:id', (request, response) => {
+    const questionId = parseInt(request.params.id);
+
+    if (Number.isInteger(questionId) && questionId >= 0 && questionId < questions.length) {
+        response.json(questions[questionId]);
+    } else {
+        response.status(404).json({ error: 'Question ' + questionId + ' not found' });
+    }
+});
 
 app.listen(3000, function () {
  console.info(`Server is running at port 3000`);
