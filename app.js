0d35a4467f0cbc72751770d3cca3aec0fc4c4e0f
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-01-18 16:14:22 +0100

    zad 10

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ const app = express();
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
+const path = require('path');
 
-app.get('/', (request, response) => {
-   response.send('Serwer działa!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.get('/', (request, response) => {
+//    response.send('Serwer działa!');
+// });
 
 // let chart1 = {"type":"line","data":{"labels":["January","February","March","April","May","June"],"datasets":[{"label":"My First dataset","backgroundColor":"rgb(255, 99, 132)","borderColor":"rgb(255, 99, 132)","data":[0,10,5,2,20,30,45]}]},"options":{}};
 
 
-// app.get('/', (request, response) => {
-//     response.render(__dirname + '/index.html',  {subject: 'Technologie webowe w aplikacjach Internetu', chart1: JSON.stringify(chart1)}
-//     )
-//  }),
+app.get('/', (request, response) => {
+    response.render(__dirname + '/index.html',  {subject: 'Technologie webowe w aplikacjach Internetu'}
+    )
+ }),
  
 
 app.listen(config.port, function () {
