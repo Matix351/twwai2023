1b69fc313f95a82b39c502a2146b239d0d2f0467
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-01-18 15:05:17 +0100

    00

---
+++ style.cc
@@ -0,0 +1,29 @@
+body, html {
+   height: 100%;
+   margin: 0;
+}
+
+nav {
+   position: fixed;
+   background-color: #fff;
+   width: 100%;
+   box-shadow: 10px 5px 5px black;
+}
+
+
+nav ul li {
+   list-style-type: none;
+   display: inline;
+   margin-right: 15px;
+}
+
+nav ul li a {
+   text-decoration: none;
+   color: #0d0fbf;
+   transition: 0.5s;
+}
+
+nav ul li a:hover {
+   text-decoration: underline;
+   color: #000;
+}
