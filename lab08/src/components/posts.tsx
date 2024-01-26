e4aeb9974021002d8947ee522ae767ca9a07ec34
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-01-26 02:40:22 +0100

    zad 1-4

---
+++ lab08/src/components/posts.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+const [data, setData] = useState(null);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+   const fetchData = async () => {
+       try {
+           const response = await fetch('http://localhost:3100/api/posts');
+           const result = await response.json();
+            //wydruk danych w konsoli
+           console.log(result)
+
+           setData(result);
+           setLoading(false);
+       } catch (error) {
+           console.error('Błąd podczas pobierania danych:', error);
+           setLoading(false);
+       }
+   };
+
+   fetchData();
+}, []);
+
+return (
+    <div>
+        {loading ? (
+            <p>Ładowanie danych...</p>
+        ) : (
+            <div>
+                <ul style={{listStyleType: "none"}}>
+                    {data.map((item, index) => (
+                        <li key={index}>
+                            <div>
+                                Tytuł: {item.title}
+                            </div>
+                            <div>
+                                {item.text}
+                            </div>
+                            <hr/>
+                        </li>
+ 
+                    ))}
+                </ul>
+            </div>
+        )}
+    </div>
+ );
